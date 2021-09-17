ea31fc18362bb133038132a426bf6b6897e39925
Kit Johnson <kit@fmail.co.uk>
2021-09-17 08:57:44 +0700

    Provide passing example

--- test/specs/example.js
+++ test/specs/example.js
@@ -1,12 +1,18 @@
 describe('Bug test', () => {
-    it('should wait until text has changed', async () => {
+    it('Hero text can be matched with toHaveText method', async () => {
+        await browser.url('./');
+        const elem = await $('.hero__subtitle')
+        expect(elem).toHaveText('Next-gen browser and mobile automation test framework for Node.js')
+    });
+
+    it('Hero text can be matched with waitUntil getText method', async () => {
         await browser.url('./');
         const elem = await $('.hero__subtitle')
         await elem.waitUntil(async () => {
-            return (await this.getText()) === 'I am now different'
+            return (await this.getText()) === 'Next-gen browser and mobile automation test framework for Node.js'
         }, {
             timeout: 5000,
-            timeoutMsg: 'expected text to be different after 5s'
+            timeoutMsg: 'Could not match text'
         });
     });
 })
